a1ddf41c1f95e70350961a9f34c87007a7428983
nikasome <kou1pomu@yahoo.co.jp>
2019-05-15 16:08:21 +0900

    Updates

---
+++ precache-manifest.2ad59d6508f26267388abc15c3050232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d9a74e1509fbd4f6c3bbbeced1699d",
+    "url": "/ReadingManagement/index.html"
+  },
+  {
+    "revision": "320b91d9d8037860c6db",
+    "url": "/ReadingManagement/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ReadingManagement/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "320b91d9d8037860c6db",
+    "url": "/ReadingManagement/static/js/main.59e55806.chunk.js"
+  },
+  {
+    "revision": "3a77667861a0901c5c8d",
+    "url": "/ReadingManagement/static/js/runtime~main.65a92166.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReadingManagement/static/media/logo.5d5d9eef.svg"
+  }
+]);
