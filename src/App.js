cba6c294e152afd2541e7251d7d02e77220f56b6
nikasome <kou1pomu@yahoo.co.jp>
2019-08-21 21:32:35 +0900

    add display user icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import firebase from './firebase.js';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <p>Good Morning</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+  login() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Name: {this.state.user && this.state.user.displayName}</p>
+        <img src={this.state.user && this.state.user.photoURL} alt="icon" />
+
+        {this.state.user ? (
+          <button onClick={this.logout}>Google logout</button>
+        ) : (
+          <button onClick={this.login}>Google Login</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
